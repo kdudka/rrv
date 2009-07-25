90ad76147075d87a79aae6a64026082d95173db7
Kamil Dudka <kdudka@redhat.com>
2009-07-26 00:26:55 +0200

    change license to GPLv3

--- src/EntitySet.h
+++ src/EntitySet.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2007 TODO
+ *
+ * This file is part of rrv (Radiosity Renderer and Visualizer).
+ *
+ * rrv is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * rrv is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rrv.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef ENTITYSET_H
 #define ENTITYSET_H
 
